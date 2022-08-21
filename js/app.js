73e44ae1f62284526e760420dbf53ff1a8b59689
ISMAIL JOSIM <ismailjosim>
2022-08-22 01:17:06 +0600

    get player name and show them into the selected area

--- js/app.js
+++ js/app.js
@@ -2,7 +2,24 @@
 const selectBtn = document.querySelectorAll('#btn-select');
 const tableBody = document.getElementById('table-body');
 
+
 const playerArr = [];
+//============> Show selected player
+const selectedPlayer = function (players) {
+    tableBody.innerHTML = ''; // make tableBody box empty
+
+    for (let i = 0; i < players.length; i++) {// looping over the players array of object
+
+        const playerName = players[i].name; // get array name.
+
+        const tableRow = document.createElement('tr'); // Create Table row(tr) element
+
+        tableRow.innerHTML = `<td>${ i + 1 }</td><td>${ playerName }</td>`; // innerHTML ==> tableRow
+
+        tableBody.appendChild(tableRow); // Show tableRow ==> tableBody
+
+    }
+};
 
 // ==========> Click Me function
 const clickMe = function (element) {
@@ -10,10 +27,10 @@ const clickMe = function (element) {
 
     const title = targetParent.children[0].innerText; // get player name
 
-    const playerObj = { playerName: title }; // make player an object.
+    const playerObj = { name: title }; // make player an object.
 
     playerArr.push(playerObj); // Push products in an array
 
-    console.log(playerArr.length);// add products length
+    selectedPlayer(playerArr);
 
 };
