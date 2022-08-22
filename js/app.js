6e280056647f2a4186c917281e5cc6ceca852080
ISMAIL JOSIM <ismailjosim>
2022-08-23 02:53:28 +0600

    footer section added

--- js/app.js
+++ js/app.js
@@ -16,77 +16,69 @@ const finalPlayerExp = document.querySelector('#total-exp');
 let countPlayer = 0;
 
 const clickMe = function (element) {
-    countPlayer += 1; // increase +1 every time call the function
-    const targetParent = element.parentNode; // target parent
-    const title = targetParent.children[0].innerText; // get player name
-    const tableRow = document.createElement('tr'); // Create Table row(tr) element
+    countPlayer += 1;
+
+    const targetParent = element.parentNode;
+    const title = targetParent.children[0].innerText;
+    const tableRow = document.createElement('tr');
+
     if (countPlayer > 5) {
         alert('Please Add only 5 players!');
     } else {
-        tableRow.innerHTML = `<tr><th>0${ countPlayer }</th><td>${ title }</td></tr>`; // innerHTML ==> tableRow
+        tableRow.innerHTML = `<tr><th>0${ countPlayer }</th><td>${ title }</td></tr>`;
         tableBody.appendChild(tableRow);
-        // disable button
         element.style.backgroundColor = "gray";
         element.setAttribute("disabled", "true");
     }
 };
-
-// get input value
+//===> get input value
 const getInputNumber = function (element) {
     const elementStr = element.value;
+    const convertNumber = parseFloat(elementStr);
+
+    if (isNaN(convertNumber) !== true && convertNumber !== 0 && convertNumber > 0) {
+        return convertNumber;
+    }
+};
+//===> get inner Text
+const getInnerNumber = function (element) {
+    const elementStr = element.innerText;
     const convertNumber = parseFloat(elementStr);
 
     if (isNaN(convertNumber) !== true && convertNumber !== 0) {
         return convertNumber;
     }
-
 };
-// get inner Text
-const getInnerNumber = function (element) {
-    const elementStr = element.innerText;
-    const convertNumber = parseFloat(elementStr);
-    if (isNaN(convertNumber) !== true && convertNumber !== 0 && convertNumber < 0) {
-        return convertNumber;
-    }
-};
-
-// calculation function
+//===> calculation function
 btnCalculate.addEventListener('click', function () {
     const selectPlayerList = document.querySelectorAll('tr');
     const playerListNumber = selectPlayerList.length;
-
-    // get per player input value
     const perPlayerCost = getInputNumber(costPerPlayer);
     const totalPlayer = playerListNumber * perPlayerCost;
 
     if (selectPlayerList.length < 1) {
         alert('Player is not added ⚠');
-
     } else if (isNaN(perPlayerCost) === true) {
-
         alert('PLEASE! Add A Valid Number ⚠');
     } else {
-
         totalPerPlayerExp.innerText = totalPlayer;
     }
 });
-
-//=========> Total Expenses Calculation Button
+//===> Total Expenses Calculation Button
 btnTotalCalc.addEventListener('click', function () {
-    const playerPerExp = getInnerNumber(totalPerPlayerExp);
-    const manager = getInputNumber(managerFees);
-    const coach = getInputNumber(coachFees);
+    const playerPerCost = getInnerNumber(totalPerPlayerExp);
+    const managerCost = getInputNumber(managerFees);
+    const coachCost = getInputNumber(coachFees);
+    const TotalBalance = playerPerCost + managerCost + coachCost;
 
-
-    const TotalBalance = playerPerExp + manager + coach;
-
-    if (isNaN(TotalBalance) === true) {
-        alert('Player Expenses is not added ⚠');
-    } else if (isNaN(TotalBalance) === true) {
-        alert('⚠');
+    if (isNaN(playerPerCost) === true) {
+        alert('Per Player Expenses is not Valid ⚠');
+    } else if (isNaN(managerCost) === true) {
+        alert('⚠ PLEASE! Add A Valid Manager Amount');
+    } else if (isNaN(coachCost) === true) {
+        alert('⚠ Coach Fees is not Valid');
     }
     else {
-
         finalPlayerExp.innerText = TotalBalance;
     }
 });
